5603a52f4595f4a0832c95169de2d3d28f03ea2d
sametacar <sametacarsamet@gmail.com>
2026-02-19 00:49:11 +0300

    mk sounds added

---
+++ play-mk.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Plays a StarCraft sound based on event type
+SOUNDS_DIR="$HOME/.claude/sounds/mortalkombatsounds"
+
+play() {
+  afplay "$SOUNDS_DIR/$1" &
+}
+
+case "$1" in
+  stop)
+    case $(( RANDOM % 2 )) in
+      2) play "impressive.mp3" ;;
+      3) play "superb.mp3" ;;
+    esac
+    ;;
+  sessionstart)
+    play "fight.mp3"
+    ;;
+  sessionend)
+    play "laughter.mp3"
+    ;;
+  
+  submit)
+    case $(( RANDOM % 3 )) in
+      0) play "rayden-torpedo.mp3" ;;
+      1) play "scorpion-get-over-here.mp3" ;;
+      2) play "fight.mp3" ;;
+    esac
+    ;;
+  question)
+     case $(( RANDOM % 2 )) in
+      0) play "fatality.mp3" ;;
+      1) play "finish-him.mp3" ;;
+    esac
+    ;;
+  *)
+    # rastgele çal
+    sounds=()
+    while IFS= read -r -d $'\0' f; do
+      sounds+=("$f")
+    done < <(find "$SOUNDS_DIR" -maxdepth 1 -type f \( -name "*.wav" -o -name "*.mp3" -o -name "*.aiff" \) -print0 2>/dev/null)
+    if [ ${#sounds[@]} -gt 0 ]; then
+      afplay "${sounds[RANDOM % ${#sounds[@]}]}" &
+    fi
+    ;;
+esac
+i-can-t-build-there
